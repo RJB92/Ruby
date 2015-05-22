ee62ffe0afe44115e23c9a30d80fa1f0e89c13fb
Ray <rjjames92@gmail.com>
2015-05-22 15:59:56 +0100

    created fortune program

---
+++ fortune.rb
@@ -0,0 +1,8 @@
+puts "tell me your name and let me read your fortune"
+name = gets .chomp
+
+if name == "ray".downcase
+	puts "I see great things in your future"
+elsif name == "Sarah"
+	puts "well it could either Beeeeeeeeeeeeeeeeeeeee, well would ya look at tha time. sai ah gatta go"
+end 
