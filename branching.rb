fdad8ef43e830b77f46e5de1362c78392f5fea3c
Ray <rjjames92@gmail.com>
2015-05-22 16:00:33 +0100

    modified branching.rb

--- branching.rb
+++ branching.rb
@@ -2,6 +2,17 @@ puts "Hello, what's your name?"
 name = gets.chomp
 puts "hello " + name + "."
 
-if name == "Chris"
+if name == "Ray".downcase
 	puts "What a lovely name!"
+end
+
+
+puts "what?"
+want = gets.chomp
+
+
+if want.include? "sex"
+	puts "leggggooooooooooooo"
+else
+	puts "nah fuck off mate"
 end
