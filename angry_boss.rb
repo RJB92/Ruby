0dd8c7ef5223fbc319f29313fec21565a02f9a85
Ray <rjjames92@gmail.com>
2015-05-25 15:23:25 +0100

    modified angry boss and test doc

--- angry_boss.rb
+++ angry_boss.rb
@@ -1,8 +1,8 @@
 puts "WHAT THE FUCK DO YOU WANT!!!!"
 want = gets.chomp
 
+
 if want == 'Less Money'.downcase
-	
 	puts "Oh sounds about fair, NOW BACK TO WORK MUTHA FUCKA!!!"
 else
 	puts "WHADYA MEAN YOU WANT " + want.upcase + "!!!!!!!!! YOOOUUURRRREEEE FIIAAAARRRDDD"
--- test.rb
+++ test.rb
@@ -5,7 +5,7 @@ puts "Table of contents".center lineWidth
 puts "Chapter 1 Numbers".ljust(lineWidth/2) + "page 1".rjust(lineWidth/2)
 puts "Chapter 2 Letters".ljust(lineWidth/2) + "page 72".rjust(lineWidth/2)
 puts "Chapter 3 Variables".ljust(lineWidth/2) + "page 118".rjust(lineWidth/2)
-=end
+
 
 
 
@@ -17,3 +17,25 @@ puts "Hi #{name}"
 if name == "Ray"
 	puts "what a lovely name"
 end
+
+
+
+puts "Hello and welcome to 74th grade English"
+puts "My name is Mrs Gabbard. and your name is?....."
+
+name = gets.chomp
+
+if name == name.capitalize
+	puts "Please take a seat " + name + "."
+else
+	puts name + "? you mean " + name.capitalize + ", right?"
+	puts "you dont know how to spell your own name?"
+	reply = gets.chomp
+
+	if reply.downcase == "yes"
+		puts "hmmph! well, sit down!"
+	else
+		puts "GET OUT!!"
+	end
+end
+=end
